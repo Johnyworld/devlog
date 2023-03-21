3f62210b6147bd718f14fe4ff0c7ebec290c67dd
johnyworld <johnyworld@naver.com>
2023-03-21 22:33:02 +0900

    README.md 파일을 불러와서 데이터로 파싱 구현(리팩토링 전)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/repos/johnyworld/dev-archive/readme")
+      .then((res) => {
+        const content = decodeURIComponent(escape(atob(res.data.content)));
+        const match = content
+          .match(/[#]{3}(.+)/g)
+          ?.map((str) => str.replace(/[#]{3} /, ""));
+        const replace = content.match(/\[(.*?)\]\((.*?)\)/g)?.map((str) => {
+          const category = content.split(str)[0].match(/[#]{3}/g)?.length || 0;
+          const [name, path] = str
+            .replace(/\[(.*?)\]\((.*?)\)/g, "$1.$2")
+            .split(".");
+          return { category: match?.[category - 1], name, path: "/" + path };
+        });
+        console.log({ match, content, replace });
+      });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,8 +34,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
