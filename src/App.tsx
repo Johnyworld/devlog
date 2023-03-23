3f89c7241266483ec0374bd553dfcc498f3e426e
johnyworld <johnyworld@naver.com>
2023-03-23 21:35:39 +0900

    Post 리스트 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { Link } from "react-router-dom";
 import useGetPosts from "./hooks/useGetPosts";
 
 function App() {
   const { data, loading } = useGetPosts();
   console.log(data, loading);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ul>
+        {data?.categories.map((category) => (
+          <li>{category}</li>
+        ))}
+      </ul>
+      <ul>
+        {data?.posts.map((post) => (
+          <li>
+            <Link to={post.path}>
+              <h3>{post.name}</h3>
+            </Link>
+            {post.desc && <p>{post.desc}</p>}
+            <p>{post.category}</p>
+            <p>{post.path}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
