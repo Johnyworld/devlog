6270b5f1a9bd26fb6ea3f28cc519f36348cb6c3c
johnyworld <johnyworld@naver.com>
2023-04-04 23:58:24 +0900

    마크다운에 anchor 컴포넌트 오버라이드

--- src/components/containers/PostViewerContainer.tsx
+++ src/components/containers/PostViewerContainer.tsx
@@ -1,5 +1,8 @@
 import useGetPost from "../../hooks/useGetPost";
+import { removeExtension } from "../../utils/stringUtils";
 import Markdown from "../views/Markdown";
+import { AnchorHTMLAttributes, DetailedHTMLProps } from "react";
+import { Link } from "react-router-dom";
 
 interface Props {
   postName: string;
@@ -16,7 +19,29 @@ const PostViewerContainer = ({ postName }: Props) => {
     return <div>No data</div>;
   }
 
-  return <Markdown>{data}</Markdown>;
+  return (
+    <Markdown
+      options={{
+        overrides: { a: OverrideAnchorByLink }
+      }}>
+      {data}
+    </Markdown>
+  );
+};
+
+const OverrideAnchorByLink = ({
+  ...props
+}: DetailedHTMLProps<
+  AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>) => {
+  return (
+    <Link
+      to={props.href != null ? removeExtension("/archive/" + props.href) : "/"}
+      target={props.target}>
+      {props.children}
+    </Link>
+  );
 };
 
 export default PostViewerContainer;
---
+++ src/utils/stringUtils.ts
@@ -0,0 +1,6 @@
+const extensions = ["md"];
+
+export const removeExtension = (fileName: string): string => {
+  const reg = new RegExp(`.(${extensions.join("|")})`);
+  return fileName.replace(reg, "");
+};
