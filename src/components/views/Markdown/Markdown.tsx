b79e9d3bc6c5d1bf007b38839721aad56208a6fb
johnyworld <johnyworld@naver.com>
2023-04-04 23:56:53 +0900

    Markdown 컴포넌트에 option prop 추가

--- src/components/views/Markdown/Markdown.tsx
+++ src/components/views/Markdown/Markdown.tsx
@@ -1,11 +1,18 @@
-import MarkdownToJSX from "markdown-to-jsx";
+import MarkdownToJSX, {
+  MarkdownToJSX as MarkdownToJSXType
+} from "markdown-to-jsx";
 
 interface Props {
   children: string;
+  options?: MarkdownToJSXType.Options;
 }
 
-const Markdown = ({ children }: Props) => {
-  return <MarkdownToJSX className="markdown">{children}</MarkdownToJSX>;
+const Markdown = ({ children, options }: Props) => {
+  return (
+    <MarkdownToJSX className="markdown" options={{ ...options }}>
+      {children}
+    </MarkdownToJSX>
+  );
 };
 
 export default Markdown;
