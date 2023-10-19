61b27458e6194abb013bf6e66ae83093d182fccf
johnyworld <johnyworld@naver.com>
2023-10-19 14:28:05 +0900

    (Next 대응) HomeScreen 을 app/page 로 교체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,44 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+import { Post } from 'type';
+import PageContent from '../components/views/layouts/PageContent';
+import Categories from '../components/views/organisms/Categories';
+import PostCards from '../components/views/organisms/PostCards';
+import { ALL_CATEGORIES_KEY } from '../utils/constants';
+import { getCategoriesFromPosts } from '../utils/post';
+
+interface Props {
+  searchParams: {
+    c: string;
+  };
+}
+
+async function getData() {
+  const res = await fetch('https://api.github.com/repos/johnyworld/dev-archive/contents/generatedList.json');
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+const parseBase64ToObject = (data: string) => {
+  return JSON.parse(Buffer.from(data, 'base64').toString());
+};
+
+export default async function Page({ searchParams }: Props) {
+  const data = await getData();
+  const posts: Post[] = parseBase64ToObject(data.content);
+  const categories = getCategoriesFromPosts(posts);
+  const currentCategory = searchParams.c || categories[0];
+
+  return (
+    <main className='home-screen'>
+      <PageContent>
+        <Categories categories={categories} />
+      </PageContent>
+      <PageContent>
+        <PostCards
+          posts={posts.filter(post => currentCategory === ALL_CATEGORIES_KEY || post.tags.includes(currentCategory))}
+        />
+      </PageContent>
+    </main>
+  );
 }
