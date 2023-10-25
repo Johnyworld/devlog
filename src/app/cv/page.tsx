c8aa5786fbe31740d5e6828b95ec707bec5ccf39
johnyworld <johnyworld@naver.com>
2023-10-25 22:43:51 +0900

    CV 페이지 작성

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <main></main>;
+import PageContent from '@components/views/layouts/PageContent';
+import Markdown from '@components/views/Markdown';
+import { CV_FILE_API_END_POINT } from '@utils/constants';
+import { parseBase64ToString } from '@utils/parseBase64ToString';
+import { Main } from '@components/views/layouts/Main';
+
+async function getData() {
+  const res = await fetch(CV_FILE_API_END_POINT);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+  const markdownContent = parseBase64ToString(data.content);
+
+  return (
+    <Main>
+      <div>
+        <PageContent>
+          <Markdown>{markdownContent}</Markdown>
+        </PageContent>
+      </div>
+    </Main>
+  );
 }
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -3,3 +3,4 @@ export const ALL_CATEGORIES_KEY = 'All';
 export const BASE_API_END_POINT = 'https://api.github.com/repos/johnyworld/dev-archive';
 export const POST_LIST_API_END_POINT = `${BASE_API_END_POINT}/contents/generatedList.json`;
 export const EACH_POST_API_END_POINT = `${BASE_API_END_POINT}/contents/vault/tech`;
+export const CV_FILE_API_END_POINT = `${BASE_API_END_POINT}/contents/vault/resume/Introduce.md`;
