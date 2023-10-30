3e1eca5bb94990365ca45a0f41816338be37bbef
johnyworld <johnyworld@naver.com>
2023-10-30 10:43:38 +0900

    feat: 네이버 검색엔진 확인 코드

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import '@style/index.scss';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
+      <head>
+        {/* naver 검색엔진 확인 */}
+        <meta name='naver-site-verification' content='ebb0335cc1fb0aeadb38243f2293b50556fc0319' />
+      </head>
       <body>
         <Header />
         {children}
