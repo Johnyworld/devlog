57ad4564fe3f67d751e58a9c6fb349b8f9ed3516
johnyworld <johnyworld@naver.com>
2023-10-30 12:16:12 +0900

    refactor: getPostList 함수를 따로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { parseBase64ToString } from '@utils/parseBase64ToString';
-import { Post } from 'type';
 import PageContent from '../components/views/layouts/PageContent';
 // import Categories from '../components/views/organisms/Categories';
 import PostCards from '../components/views/organisms/PostCards';
 import { ALL_CATEGORIES_KEY, POST_LIST_API_END_POINT } from '../utils/constants';
 // import { getCategoriesFromPosts } from '../utils/post';
 import { Main } from '@components/views/layouts/Main';
+import { getPostList } from 'src/calls/getPostList';
 
 interface Props {
   searchParams: {
@@ -13,21 +12,8 @@ interface Props {
   };
 }
 
-async function getData() {
-  const res = await fetch(POST_LIST_API_END_POINT);
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-  return res.json();
-}
-
-const parseBase64ToObject = (data: string) => {
-  return JSON.parse(parseBase64ToString(data));
-};
-
 export default async function Page({ searchParams }: Props) {
-  const data = await getData();
-  const posts: Post[] = parseBase64ToObject(data.content);
+  const posts = await getPostList();
   // const categories = getCategoriesFromPosts(posts);
   const currentCategory = searchParams.c || ALL_CATEGORIES_KEY;
 
---
+++ src/calls/getPostList.ts
@@ -0,0 +1,20 @@
+import { POST_LIST_API_END_POINT } from '@utils/constants';
+import { parseBase64ToString } from '@utils/parseBase64ToString';
+import { Post } from 'type';
+
+export const getPostList = async (): Promise<Post[]> => {
+  const json = await getJson();
+  return parseBase64ToObject(json.content);
+};
+
+const getJson = async () => {
+  const res = await fetch(POST_LIST_API_END_POINT);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+};
+
+const parseBase64ToObject = (data: string) => {
+  return JSON.parse(parseBase64ToString(data));
+};
