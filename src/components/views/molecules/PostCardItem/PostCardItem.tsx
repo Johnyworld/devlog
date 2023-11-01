546cf1a2b0a1acf9e41c40b40644a54ee0eaeacc
johnyworld <johnyworld@naver.com>
2023-11-01 10:59:30 +0900

    feat: PostCardItem 에도 태그 추가

--- src/components/views/molecules/PostCardItem/PostCardItem.tsx
+++ src/components/views/molecules/PostCardItem/PostCardItem.tsx
@@ -1,6 +1,8 @@
 import { Post } from 'type';
 import style from './PostCardItem.module.scss';
 import { formatISODatePart } from '@utils/string';
+import Link from 'next/link';
+import { getRoute } from '@utils/routes';
 
 interface Props {
   post: Post;
@@ -10,7 +12,11 @@ const PostCardItem = ({ post }: Props) => {
   return (
     <div className={style.postCardItem}>
       <h3 className={style.postCardItem_title}>{post.title}</h3>
-      <p className={style.postCardItem_createdAt}>{formatISODatePart(post.createdAt)}</p>
+      <p className={style.postCardItem_createdAt}>
+        {formatISODatePart(post.createdAt)}
+        {' · '}
+        {post.tags.join(', ')}
+      </p>
     </div>
   );
 };
