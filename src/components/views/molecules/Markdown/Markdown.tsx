795c6f6a104249131753c778e5db4d907b30c81e
johnyworld <johnyworld@naver.com>
2023-11-03 22:06:19 +0900

    refactor: 마크다운 OverrideAnchor 컴포넌트 분리

--- src/components/views/molecules/Markdown/Markdown.tsx
+++ src/components/views/molecules/Markdown/Markdown.tsx
@@ -1,9 +1,6 @@
 import MarkdownToJSX, { MarkdownToJSX as MarkdownToJSXType } from 'markdown-to-jsx';
 import style from './Markdown.module.scss';
-import { AnchorHTMLAttributes, DetailedHTMLProps } from 'react';
-import Link from 'next/link';
-import { removeExtension } from '@utils/stringUtils';
-import { getRoute } from '@utils/routes';
+import { OverrideAnchorByLink } from './overrides/OverrideAnchorByLink';
 
 interface Props {
   children: string;
@@ -18,31 +15,6 @@ const Markdown = ({ children, options }: Props) => {
     >
       {children}
     </MarkdownToJSX>
-  );
-};
-
-const OverrideAnchorByLink = ({
-  ...props
-}: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => {
-  // 경로가 없다면 홈으로 보낸다.
-  if (!props.href) {
-    return <Link href='/'>{props.children}</Link>;
-  }
-
-  // 외부 경로인 경우 새창으로 띄워준다.
-  if (/https?:\/\//.test(props.href)) {
-    return (
-      <Link href={props.href} target='_blank'>
-        {props.children}
-      </Link>
-    );
-  }
-
-  // 앱 내 이동시.
-  return (
-    <Link href={removeExtension(getRoute.post() + '/' + props.href)} target={props.target}>
-      {props.children}
-    </Link>
   );
 };
 
