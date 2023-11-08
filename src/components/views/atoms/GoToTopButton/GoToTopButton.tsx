a5613862f46db7a6ad3beb974884122be8bf561a
johnyworld <johnyworld@naver.com>
2023-11-08 22:27:02 +0900

    fix: GoToTop 버튼 반응형 스타일 변경

--- src/components/views/atoms/GoToTopButton/GoToTopButton.tsx
+++ src/components/views/atoms/GoToTopButton/GoToTopButton.tsx
@@ -9,9 +9,9 @@ export const GoToTopButton = ({ ...props }: Props) => {
   return (
     <PageContent className={style.goToTopButton_Positioner}>
       <button {...props} className={classNames(style.goToTopButton, props.className)}>
-        <svg width='22' height='22' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
-          <path d='M3 8L9 2L15 8' stroke='var(--color-gray-strong)' stroke-width='1' />
-          <path d='M9 2V15.5' stroke='var(--color-gray-strong)' stroke-width='1' />
+        <svg width='26' height='26' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
+          <path d='M3 8L9 2L15 8' stroke='var(--color-letter)' stroke-width='1' />
+          <path d='M9 2V15.5' stroke='var(--color-letter)' stroke-width='1' />
         </svg>
       </button>
     </PageContent>
