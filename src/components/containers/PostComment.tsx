a9d528fb35ebff0b5a12b9e9472002bb62c5a7f0
johnyworld <johnyworld@naver.com>
2023-11-10 17:39:53 +0900

    feat: github comment 에도 다크모드 적용

--- src/components/containers/PostComment.tsx
+++ src/components/containers/PostComment.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { getCurrentTheme } from '@utils/theme';
+
 export const PostComment = () => {
   return (
     <div
@@ -13,7 +15,7 @@ export const PostComment = () => {
         scriptElem.async = true;
         scriptElem.setAttribute('repo', 'johnyworld/devlog');
         scriptElem.setAttribute('issue-term', 'pathname');
-        scriptElem.setAttribute('theme', 'github-light');
+        scriptElem.setAttribute('theme', `github-${getCurrentTheme()}`); // TODO: 상태관리 필요. 지금은 모드를 바꿔도 바로 반영이 안됨
         scriptElem.setAttribute('label', 'comments');
         scriptElem.crossOrigin = 'anonymous';
         elem.replaceChildren(scriptElem);
