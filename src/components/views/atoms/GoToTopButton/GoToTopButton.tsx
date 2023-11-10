4e12d1db26cb693180798ffcf105b9150ee2c001
johnyworld <johnyworld@naver.com>
2023-11-11 00:00:55 +0900

    fix: GoToTop 버튼 위치 버그 픽스

--- src/components/views/atoms/GoToTopButton/GoToTopButton.tsx
+++ src/components/views/atoms/GoToTopButton/GoToTopButton.tsx
@@ -7,13 +7,13 @@ interface Props extends Omit<HTMLAttributes<HTMLButtonElement>, 'children'> {}
 
 export const GoToTopButton = ({ ...props }: Props) => {
   return (
-    <PageContent className={style.goToTopButton_Positioner}>
+    <div className={style.goToTopButton_Positioner}>
       <button {...props} className={classNames(style.goToTopButton, props.className)}>
         <svg width='26' height='26' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
           <path d='M3 8L9 2L15 8' stroke='var(--color-letter)' strokeWidth='1' />
           <path d='M9 2V15.5' stroke='var(--color-letter)' strokeWidth='1' />
         </svg>
       </button>
-    </PageContent>
+    </div>
   );
 };
