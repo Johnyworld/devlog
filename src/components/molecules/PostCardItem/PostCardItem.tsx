f973799c31ac69cb05e917699d1fb34b0a302f6b
johnyworld <johnyworld@naver.com>
2023-11-20 21:02:22 +0900

    fix: 폰트 색상 픽스

--- src/components/molecules/PostCardItem/PostCardItem.tsx
+++ src/components/molecules/PostCardItem/PostCardItem.tsx
@@ -9,7 +9,7 @@ const PostCardItem = ({ post }: Props) => {
   return (
     <div className="post-card-item rounded px-2.5 py-1.5 -mx-2.5 _clickable">
       <h3 className="text-md whitespace-nowrap _ellipsis">{post.title}</h3>
-      <p className="text-sm tet-gray mt-0.5">
+      <p className="text-sm text-gray mt-0.5">
         {formatISODatePart(post.createdAt)}
         {' · '}
         {post.tags.join(', ')}
