5998215aed658f4bb12f8238a7e8ea8d5ae94aca
Johny Kim <johnyworld@naver.com>
2024-08-04 14:52:55 +0900

    feat: 스크립트 기본 설정 추가

--- src/scripts/gen-posts.ts
+++ src/scripts/gen-posts.ts
@@ -1,1 +1,22 @@
-console.log('Hello from getPosts.ts');
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
+
+const config = {
+  vaultSrc: process.env.NEXT_PUBLIC_VAULT_SRC,
+  publishTag: '#블로그발행',
+};
+
+// 1. 전체 md 파일 path 가져오기
+// 2. 각 md 파일을 차례대로 읽으며 publishTag가 있는 파일들만 따로 분류
+// 3. 해당 파일들을 posts 경로로 복사
+// 4. 해당 파일들의 리스트에 대한 json 파일 생성 title, content, path, createdAt, modifiedAt, tags
+
+const main = () => {
+  if (!config.vaultSrc) {
+    return;
+  }
+  // Do something
+};
+
+main();
