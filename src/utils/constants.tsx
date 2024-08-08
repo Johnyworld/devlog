d53604ea0cc3841bae5465e302c3fb5e1df9fae6
Johny Kim <johnyworld@naver.com>
2024-08-08 14:35:16 +0900

    feat: 데이터 가져오는 레포 변경

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,9 +1,9 @@
 export const ALL_CATEGORIES_KEY = 'All';
 
-export const BASE_API_END_POINT = 'https://api.github.com/repos/johnyworld/dev-archive';
-export const POST_LIST_API_END_POINT = `${BASE_API_END_POINT}/contents/generatedList.json`;
-export const EACH_POST_API_END_POINT = `${BASE_API_END_POINT}/contents/vault/tech`;
-export const EACH_WORK_API_END_POINT = `${BASE_API_END_POINT}/contents/vault/resume/work`;
-export const CV_FILE_API_END_POINT = `${BASE_API_END_POINT}/contents/vault/resume/Introduce.md`;
+export const BASE_API_END_POINT = 'https://api.github.com/repos/johnyworld/devlog';
+export const POST_LIST_API_END_POINT = `${BASE_API_END_POINT}/contents/src/data/posts.json`;
+export const EACH_POST_API_END_POINT = `${BASE_API_END_POINT}/contents/src/data/posts`;
+export const EACH_WORK_API_END_POINT = `${BASE_API_END_POINT}/contents/src/data/work`;
+export const CV_FILE_API_END_POINT = `${BASE_API_END_POINT}/contents/src/data/Introduce.md`;
 
 export const DOMAIN_URL = 'https://johnykim.me';
