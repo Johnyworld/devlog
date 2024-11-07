2122baa0e5672f0c1a82e97491abb3bb77841afc
Johny Kim <johnyworld@naver.com>
2024-11-07 09:24:14 +0900

    feat: md 파일의 tailwind 스타일도 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{tsx,ts,js}'],
+  content: ['./src/**/*.{tsx,ts,js,md}'],
   theme: {
     colors: {
       primary: 'var(--color-primary)',
